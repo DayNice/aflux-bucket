09d959c2e6ca2078a754cb9b4209ad2c80e9321f
DayNice <76981513+DayNice@users.noreply.github.com>
2026-06-21 01:09:27 +0900

    refactor: make cli.__init__ slim

--- src/aflux_bucket/cli/__init__.py
+++ src/aflux_bucket/cli/__init__.py
@@ -1,9 +1,3 @@
-from cyclopts import App
+from ._main import app
 
-from ._s3 import app as s3_app
-
-app = App(name="aflux-bucket", help="Interact with object storage buckets.")
-app.command(s3_app, name="s3")
-
-if __name__ == "__main__":
-    app()
+__all__ = ["app"]

9a9022d4aacdc9c6605fb93a4c7454b3dcfd44f5
DayNice <76981513+DayNice@users.noreply.github.com>
2026-07-14 16:16:07 +0900

    feat: register cli completion command

--- src/aflux_bucket/cli/_main.py
+++ src/aflux_bucket/cli/_main.py
@@ -3,6 +3,7 @@ from cyclopts import App
 from ._s3 import app as s3_app
 
 app = App(name="aflux-bucket", help="Interact with object storage buckets.")
+app.register_install_completion_command()
 app.command(s3_app, name="s3")
 
 if __name__ == "__main__":
